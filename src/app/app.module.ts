1b87276704431be1e679f6e4c3b4ace8b40af4a3
Saba Kveladze <sabaqveladze16@gmail.com>
2024-03-07 17:21:25 +0400

    FeedBacks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { Ilustration1Component } from './ilustration1/ilustration1.component';
 import { InfoSectionComponent } from './info-section/info-section.component';
 import { Illustration2Component } from './illustration2/illustration2.component';
-import { GrayBackgroundComponent } from './gray-background/gray-background.component';
+
 import { ProductPlansComponent } from './product-plans/product-plans.component';
 import { MapAndSocialsComponent } from './map-and-socials/map-and-socials.component';
 import { FeedbacksComponent } from './feedbacks/feedbacks.component';
@@ -18,7 +18,7 @@ import { FeedbacksComponent } from './feedbacks/feedbacks.component';
     Ilustration1Component,
     InfoSectionComponent,
     Illustration2Component,
-    GrayBackgroundComponent,
+
     ProductPlansComponent,
     MapAndSocialsComponent,
     FeedbacksComponent
